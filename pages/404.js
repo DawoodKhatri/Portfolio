0a9d67cce3ec5a031c802f37f9d28540c9bd1b30
DawoodKhatri <dawoodkhatri18@gmail.com>
2022-12-23 11:58:44 +0530

    Add files via upload

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,17 @@
 import Jumbo from "../components/Jumbo";
 import Title from "../components/Title";
 import Paragraph from "../components/Paragraph";
+import { useEffect } from "react";
 
 export default function notfound() {
+  useEffect(() => {
+    window.location.href = window.location.href.toLowerCase();
+  });
   return (
     <>
-      <Jumbo className="text-primary" text="404"/>
-      <Title text="Page not found"/>
-      <Paragraph text="The page you are looking for does not exist."/>
+      <Jumbo className="text-primary" text="404" />
+      <Title text="Page not found" />
+      <Paragraph text="The page you are looking for does not exist." />
     </>
   );
 }
