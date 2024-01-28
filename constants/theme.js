93af14b3766e5f62097c33209673f301df6dc359
Dawood Khatri <dawoodkhatri18@gmail.com>
2024-01-28 22:41:52 +0530

    Updated App Theme

--- constants/theme.js
+++ constants/theme.js
@@ -1,14 +1,3 @@
 import colors from "tailwindcss/colors";
 
-const appColors = [
-  colors.amber,
-  colors.cyan,
-  colors.emerald,
-  colors.sky,
-  colors.rose,
-  colors.teal,
-  colors.yellow,
-];
-
-export const APP_COLOR =
-  appColors[Math.floor(Math.random() * appColors.length)];
+export const APP_COLOR = colors.purple;
