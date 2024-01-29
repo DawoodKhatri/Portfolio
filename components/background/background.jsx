8fa6addc6b54988b2f3aca8324fcf43177176808
Dawood Khatri <dawoodkhatri18@gmail.com>
2024-01-29 20:13:54 +0530

    Updated Background

--- components/background/background.jsx
+++ components/background/background.jsx
@@ -5,7 +5,7 @@ import WeatherScene from "./weatherScene";
 
 const AppBackground = () => {
   return (
-    <div className="w-full h-screen fixed -z-50 bg-zinc-900">
+    <div className="w-full h-screen fixed -z-50 bg-[linear-gradient(to_right,rgba(0,0,0,0.25),rgba(0,0,0,0.75)),url('/background.jpg')] bg-no-repeat bg-cover bg-center">
       <Canvas>
         <WeatherScene />
       </Canvas>
